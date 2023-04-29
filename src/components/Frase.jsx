35044e25a20f49f7eb95cc31180509889c04173a
Bernardo Burgués <burguesbernardo@gmail.com>
2023-04-29 00:18:54 -0300

    shared buttons

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -7,7 +7,7 @@ const Frase = ({ frase }) => {
       className="whitespace-pre-line p-3 text-xl lg:text-[22px] font-semibold text-center mb-3"
       style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 2)" }}
     >
-      🎶 {frase} 🎶
+      🎶{frase}🎶
     </p>
   );
 };
